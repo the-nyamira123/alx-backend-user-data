f22b8a74fa23f67500ab792697c3cefde07cf4b5
the-nyamira123 <nathanslay7@gmail.com>
2024-11-09 15:00:24 +0000

    tugange yajayo

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -11,7 +11,8 @@ from typing import List
 patterns = {
     'extract': lambda x, y: r'(?P<field>{})=[^{}]*'.format('|'.join(x), y),
     'replace': lambda x: r'\g<field>={}'.format(x),
-} "password")
+}
+PII_FIELDS = ("name", "email", "phone", "ssn", "password")
 
 
 def filter_datum(
